02bf5016b2981a789b515218306f34eaf12f5c84
Hwang Chaerin <rin2005@hanmail.net>
2025-03-19 18:23:20 +0900

    inputTest.py

--- inputtest.py
+++ inputtest.py
@@ -4,20 +4,26 @@ num1 = int(num1)
 num2 = input("숫자2 ==> ")
 num2 = int(num2)
 
+#합
 numSum = num1 + num2
 print(num1, '+', num2, '=', numSum)
 
+#차
 numDiff = num1 - num2
 print(num1, '-', num2, '=', numDiff)
 
+#곱
 numProd = num1 * num2
 print(num1, '*', num2, '=', numProd)
 
+#몫
 numQuot = num1 / num2
 print(num1, '/', num2, '=', numQuot)
 
+#나머지
 numRem = num1 % num2
 print(num1, '%', num2, '=', numRem)
 
+#거듭제곱
 numPow = num1 ** num2
 print(num1, '**', num2, '=', numPow)
