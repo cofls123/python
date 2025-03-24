9eafe256864f73199c06319454d2e81d0bac1108
Hwang Chaerin <rin2005@hanmail.net>
2025-03-24 18:45:59 +0900

    CU_기본 세팅팅

--- Divider.py
+++ Divider.py
@@ -6,8 +6,8 @@ def btn_click():
     num1 = int(entry1.get())
     num2 = int(entry2.get())
 
-    str1 = str(num1) + '을(를)' + str(num2) + '(으)로 나눈 몫은' + str(num1//num2) + '입니다.'
-    str2 = str(num1) + '을(를)' + str(num2) + '(으)로 나눈 나머지은' + str(num1%num2) + '입니다.'
+    str1 = str(num1) + '을(를) ' + str(num2) + '(으)로 나눈 몫은' + str(num1//num2) + '입니다.'
+    str2 = str(num1) + '을(를) ' + str(num2) + '(으)로 나눈 나머지은' + str(num1%num2) + '입니다.'
 
 
     labelRest1 = tkinter.Label(root,text=str1, font=('맑은고딕',10))
@@ -16,22 +16,29 @@ def btn_click():
     labelRest2 = tkinter.Label(root,text=str2, font=('맑은고딕',10))
     labelRest2.place(x=21,y=110)
 
+#기본 틀
 root = tkinter.Tk()
 root.title('산술 연산자')
 root.geometry('400x400')
 
+
+#글자
 label1 = tkinter.Label(root, text='나눠지는 수', font=('맑은고딕', 10))
 label2 = tkinter.Label(root, text='나누는 수', font=('맑은고딕', 10))
 
 label1.place(x=20,y=20)
 label2.place(x=25,y=45)
 
+
+#입력칸
 entry1 = tkinter.Entry(width=8)
 entry2 = tkinter.Entry(width=8)
 
 entry1.place(x=102,y=20)
 entry2.place(x=102,y=48)
 
+
+#버튼
 btn = tkinter.Button(root, text='계산', font=('맑은고딕',10), command=btn_click)
 btn.place(x=186,y=20,width=54,height=48)
 
--- cu.py
+++ cu.py
@@ -2,8 +2,18 @@ import tkinter
 
 root = tkinter.Tk()
 root.title('CU')
-root.geometry('800x400')
+root.geometry('900x400')
 
+
+def btn_click():
+    N_num1 = int(entry1.get())
+    N_num2 = int(entry2.get())
+    N_num2 = int(entry2.get())
+    N_num2 = int(entry2.get())
+    N_num2 = int(entry2.get())
+    N_num2 = int(entry2.get())
+
+#물품
 D1 = tkinter.Label(root, text='캔 커피', font=('맑은고딕', 10))
 D2 = tkinter.Label(root, text='삼각김밥', font=('맑은고딕', 10))
 D3 = tkinter.Label(root, text='바나나 우유', font=('맑은고딕', 10))
@@ -11,18 +21,59 @@ D4 = tkinter.Label(root, text='도시락', font=('맑은고딕', 10))
 D5 = tkinter.Label(root, text='콜라', font=('맑은고딕', 10))
 D6 = tkinter.Label(root, text='새우깡', font=('맑은고딕', 10))
 
+#수량
 N1 = tkinter.Label(root, text='판매 수량', font=('맑은고딕', 10))
 N2 = tkinter.Label(root, text='구매 수량', font=('맑은고딕', 10))
 
 
-D1.place(x=100,y=40)
-D2.place(x=160,y=40)
-D3.place(x=250,y=40)
-D4.place(x=310,y=40)
-D5.place(x=380,y=40)
-D6.place(x=420,y=40)
+#물품
+D1.place(x=125,y=40)
+D2.place(x=240,y=40)
+D3.place(x=350,y=40)
+D4.place(x=485,y=40)
+D5.place(x=610,y=40)
+D6.place(x=715,y=40)
 
-N1.place(x=20,y=80)
+#수량
+N1.place(x=20,y=100)
 N2.place(x=20,y=150)
+
+
+#판매 수량
+D1_N1 = tkinter.Entry(width=4)
+D2_N1 = tkinter.Entry(width=4)
+D3_N1 = tkinter.Entry(width=4)
+D4_N1 = tkinter.Entry(width=4)
+D5_N1 = tkinter.Entry(width=4)
+D6_N1 = tkinter.Entry(width=4)
+
+#구매 수량
+D1_N2 = tkinter.Entry(width=4)
+D2_N2 = tkinter.Entry(width=4)
+D3_N2 = tkinter.Entry(width=4)
+D4_N2 = tkinter.Entry(width=4)
+D5_N2 = tkinter.Entry(width=4)
+D6_N2 = tkinter.Entry(width=4)
+
+
+#판매 수량
+D1_N1.place(x=130,y=100)
+D2_N1.place(x=250,y=100)
+D3_N1.place(x=370,y=100)
+D4_N1.place(x=490,y=100)
+D5_N1.place(x=610,y=100)
+D6_N1.place(x=720,y=100)
+
+#구매 수량
+D1_N2.place(x=130,y=150)
+D2_N2.place(x=250,y=150)
+D3_N2.place(x=370,y=150)
+D4_N2.place(x=490,y=150)
+D5_N2.place(x=610,y=150)
+D6_N2.place(x=720,y=150)
+
+btn = tkinter.Button(root, text='계산', font=('맑은고딕',10), command=btn_click)
+btn.place(x=80,y=200,width=700,height=30)
+
 
 root.mainloop()
