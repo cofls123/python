44b385bba87a46c956f4eb569c24f88e56ae8844
Hwang Chaerin <rin2005@hanmail.net>
2025-03-31 10:25:19 +0900

    주석_계산기기

--- Divider.py
+++ Divider.py
@@ -2,14 +2,17 @@
 #콤마는 구분하는 것/ 점은 속해있는 것 (tkinter.Tk)
 import tkinter
 
+#버튼 클릭하면 실행하는 함수
 def btn_click():
+    #값 입력 (글자여서 숫자로 바꿔야 함)
     num1 = int(entry1.get())
     num2 = int(entry2.get())
 
+    #라벨에 넣을 문자열
     str1 = str(num1) + '을(를) ' + str(num2) + '(으)로 나눈 몫은' + str(num1//num2) + '입니다.'
     str2 = str(num1) + '을(를) ' + str(num2) + '(으)로 나눈 나머지은' + str(num1%num2) + '입니다.'
 
-
+    #라벨 만들기 (결과가 담긴 문자열 1, 2)
     labelRest1 = tkinter.Label(root,text=str1, font=('맑은고딕',10))
     labelRest1.place(x=21,y=86)
 
@@ -17,13 +20,13 @@ def btn_click():
     labelRest2.place(x=21,y=110)
 
 
-#기본 틀
+#tkinter 기본문
 root = tkinter.Tk()
 root.title('산술 연산자')
 root.geometry('400x400')
 
 
-#글자
+#라벨 만들기 (설명이이 담긴 문자열 1, 2)
 label1 = tkinter.Label(root, text='나눠지는 수', font=('맑은고딕', 10))
 label2 = tkinter.Label(root, text='나누는 수', font=('맑은고딕', 10))
 
@@ -39,9 +42,9 @@ entry1.place(x=102,y=20)
 entry2.place(x=102,y=48)
 
 
-#버튼
+#계산 버튼 생성
 btn = tkinter.Button(root, text='계산', font=('맑은고딕',10), command=btn_click)
 btn.place(x=186,y=20,width=54,height=48)
 
-
+#화면 유지지
 root.mainloop()
