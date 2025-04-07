4312eb45d134a53b76bf1c8c4b32195f75e46c37
Hwang Chaerin <rin2005@hanmail.net>
2025-04-07 11:01:50 +0900

    제비뽑기

--- CanvasTest.py
+++ CanvasTest.py
@@ -16,14 +16,14 @@ canvas.create_image(400,300,image=bgimg) #스티커 붙이기(중심점 설정)
 #root - canvas - bgimg 연결성 인식(객체지향)
 
 
+
 #랜덤 라벨 문자열
 def click_btn():
     label['text']=random.choice(['대길','중길','소길','흉'])
-    
+
 
 
 #이벤트 생성(마우스 커서 좌표)
-
 def mouseMove(event):
     x=event.x
     y=event.y
@@ -36,7 +36,6 @@ labelMouse = tkinter.Label(root, text=', ', font=('맑은고딕',10))
 
 
 
-
 #라벨 생성
 label = tkinter.Label(root, text='??', font=('맑은고딕', 130))
 label.place(x=333 ,y=66 ,width=400, height=200)
@@ -44,6 +43,7 @@ label.place(x=333 ,y=66 ,width=400, height=200)
 #버튼 생성
 bnt = tkinter.Button(root, text='제비뽑기', font=('맑은고딕', 30), fg='skyblue', command=click_btn)
 bnt.place(x=339 ,y=370 ,width=300, height=100)
+
 
 
 root.mainloop()
