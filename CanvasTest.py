788850b2b57be346ce86e783bdaccea5ffdef129
Hwang Chaerin <rin2005@hanmail.net>
2025-04-07 10:51:15 +0900

    0407

--- CanvasTest.py
+++ CanvasTest.py
@@ -4,55 +4,46 @@ import random
 
 root = tkinter.Tk()
 root.title = ('캔버스 만들기')
+
+#캔버스 생성 / root랑 관련된 canvas를 만들다(객체지향코드)
 canvas = tkinter.Canvas(root, width=800, height=600, bg="skyblue")
+canvas.pack() #알아서 배치함 (0,0)
+
+#캔버스 내 이미지 생성성(bgimg 변수)
+bgimg = tkinter.PhotoImage(file="miko.png")
+canvas.create_image(400,300,image=bgimg) #스티커 붙이기(중심점 설정)
+
+#root - canvas - bgimg 연결성 인식(객체지향)
 
 
 #랜덤 라벨 문자열
-def bnt_click():
+def click_btn():
+    label['text']=random.choice(['대길','중길','소길','흉'])
     
-    Rs.place_forget()
-
-    Bgil = tkinter.Label(root, text='대길', font=('맑은고딕', 100))
-    Mgil = tkinter.Label(root, text='중길', font=('맑은고딕', 100))
-    Gil = tkinter.Label(root, text='길', font=('맑은고딕', 100))
-    Hung = tkinter.Label(root, text='흉길', font=('맑은고딕', 100))
-
-    Bgil.place(x=315 ,y=75 ,width=350, height=200)
-    Mgil.place(x=315 ,y=75 ,width=350, height=200)
-    Gil.place(x=385 ,y=66 ,width=200, height=200)
-    Hung.place(x=385 ,y=66 ,width=200, height=200)
-
-    List=[Bgil, Mgil, Gil, Hung]
-    Result=random.choice(List)
 
 
-
-
-#이벤트 생성(좌표)
+#이벤트 생성(마우스 커서 좌표)
 
 def mouseMove(event):
     x=event.x
     y=event.y
 
     labelMouse.config(text=str(x)+', '+str(y))
-    labelMouse.place(x=0,y=90)
+    labelMouse.place(x=743,y=567)
 
 root.bind('<Motion>', mouseMove)
-labelMouse = tkinter.Label(root, text='제비뽑기', font=('맑은고딕', 10))
+labelMouse = tkinter.Label(root, text=', ', font=('맑은고딕',10))
 
 
-#스티커 만들기
-bgimg = tkinter.PhotoImage(file="miko.png")
-#스티커 붙이기(중심점 설정)
-canvas.create_image(400,300,image=bgimg)
 
-#라벨 만들기
-Rs = tkinter.Label(root, text='??', font=('맑은고딕', 130))
-bnt = tkinter.Button(root, text='제비뽑기', font=('맑은고딕', 30), command=bnt_click)
 
-#라벨 붙이기
-Rs.place(x=385 ,y=66 ,width=200, height=200)
+#라벨 생성
+label = tkinter.Label(root, text='??', font=('맑은고딕', 130))
+label.place(x=333 ,y=66 ,width=400, height=200)
+
+#버튼 생성
+bnt = tkinter.Button(root, text='제비뽑기', font=('맑은고딕', 30), fg='skyblue', command=click_btn)
 bnt.place(x=339 ,y=370 ,width=300, height=100)
 
-canvas.pack() #알아서 배치함 (0,0)
+
 root.mainloop()
