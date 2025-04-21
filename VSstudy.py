5bf1c9e9a8b85860acc9d989089a4dfd34ddaaba
Hwang Chaerin <rin2005@hanmail.net>
2025-04-21 10:55:41 +0900

    연습

---
+++ VSstudy.py
@@ -0,0 +1,41 @@
+
+#1. 계산기 만들기
+
+#num1 = int(input('숫자1 ==> '))
+#num2 = int(input('숫자2 ==> '))
+
+#print(num1, '+', num2, '=' ,num1+num2)
+#print(num1, '-', num2, '=' ,num1-num2)
+#print(num1, '*', num2, '=' ,num1*num2)
+#print(num1, '/', num2, '=' ,num1/num2)
+#print(num1, '%', num2, '=' ,num1%num2)
+#print(num1, '**', num2, '=' ,num1**num2)
+
+
+
+#2. 택배 보내기
+
+#print('## 택배를 보내기 위한 정보를 입력하세요. ##')
+#Name = input('받는 사람 : ')
+#J = input('주소 : ')
+#G = int(input('무게(g) : '))
+
+#print('받는 사람 ==> ', Name)
+#print('주소 ==> ', J)
+#print('배송비 ==> ', G*5, '원')
+
+#문자열 거꾸로 출력
+#strIn = input('원본 문자열 ==> ')  #N자리열
+#strOut = strIn[3]+strIn[2]+strIn[1]+strIn[0]
+##반복문!으로 수정하기!!!!
+#strOut = 
+#n = len(strIn)
+#print(n)
+#print('반대 문자열 ==>', strOut)
+
+#19세 미만이면 집에 갈 시간이네요. 나이 입력 받고 조건 걸기기(조건문)
+#N = int(input('나이를 입력 ==> '))
+#if N<19:
+#    print('집에 갈 시간이네요!')
+#    print('협조 감사합니다.')
+
